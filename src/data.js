6065c5cc90a184a3ff17220512ec08d72fda2197
Artem Tyurin <spacevillain@ya.ru>
2012-10-14 23:52:19 +0400

    data fixes

--- src/data.js
+++ src/data.js
@@ -23,70 +23,85 @@ var authors = [
 ];
 
 var lections = [
-{date: "15.09.12", title: "Общий цикл разработки", authorName: "Михаил Трошев"},
+{date: "15.09.2012 19:00", title: "Общий цикл разработки", authorName: "Михаил Трошев"},
 
-{date: "15.09.12", title: "Task Tracker, Wiki", authorName: "Сергей Бережной",
+{date: "15.09.2012 20:00", title: "Task Tracker, Wiki", authorName: "Сергей Бережной",
 note: "shmakowiki, mediawiki"},
 
-{date: "18.09.12", title: "Командная строка Unix", authorName: "Виктор Ашик",
+
+{date: "18.09.2012 19:00", title: "Командная строка Unix", authorName: "Виктор Ашик",
 note: "whoami, man, apropos, xargs"},
 
-{date: "18.09.12", title: "Редакторы кода", authorName: "Вячеслав Олиянчук",
+{date: "18.09.2012 20:00", title: "Редакторы кода", authorName: "Вячеслав Олиянчук",
 note: "Textmate, Sublime Text 2, Webstorm, VIM"},
 
-{date: "20.09.12", title: "Браузеры", authorName: "Георгий Мостоловица", note:
+
+{date: "20.09.2012 19:00", title: "Браузеры", authorName: "Георгий Мостоловица", note:
 "Mosaic, Netscape, Internet Explorer, Opera, Firefox, Safari, Chrome"},
 
-{date: "20.09.12", title: "Системы контроля версий", authorName: "Сергей Сергеев",
+{date: "20.09.2012 20:00", title: "Системы контроля версий", authorName: "Сергей Сергеев",
 note: "git, svn, cvs"},
 
-{date: "22.09.12", title: "Тестирование", authorName: "Марина Широчкина",
+
+{date: "22.09.2012 12:00", title: "Тестирование", authorName: "Марина Широчкина",
 note: "Яндекс.Танк"},
 
-{date: "22.09.12", title: "Развертывание верстки", authorName: "Павел Пушкарев"},
+{date: "22.09.2012 13:00", title: "Развертывание верстки", authorName: "Павел Пушкарев"},
 
-{date: "22.09.12", title: "HTTP-протокол", authorName: "Алексей Бережной",
+{date: "22.09.2012 14:00", title: "HTTP-протокол", authorName: "Алексей Бережной",
 note: "GET POST HEAD UPDATE PUT DELETE"},
 
-{date: "24.09.12", title: "XSLT (факультативная)", authorName: "Сергей Пузанков",
+
+{date: "24.09.2012 19:00", title: "XSLT (факультативная)", authorName: "Сергей Пузанков",
 note: "xml, предикат"},
 
-{date: "25.09.12", title: "Механизм работы браузера", authorName: "Роман Комаров", note:
+
+{date: "25.09.2012 19:00", title: "Механизм работы браузера", authorName: "Роман Комаров", note:
 "reflow"},
 
-{date: "25.09.12", title: "Кеширование на клиенте и сервере", authorName: "Егор Львовский"},
+{date: "25.09.2012 20:00", title: "Кеширование на клиенте и сервере", authorName: "Егор Львовский"},
 
-{date: "27.09.12", title: "Безопасность веб-приложений", authorName: "Тарас Иващенко"},
 
-{date: "27.09.12", title: "Языки программирования", authorName: "Алексей Воинов",
+{date: "27.09.2012 19:00", title: "Безопасность веб-приложений", authorName: "Тарас Иващенко"},
+
+{date: "27.09.2012 20:00", title: "Языки программирования", authorName: "Алексей Воинов",
 note: "Javascript, C++, C#, Lisp, Scheme, Clojure"},
-{date: "29.09.12", title: "JS. Базовые знания", authorName: "Михаил Давыдов"},
 
-{date: "29.09.12", title: "Транспорт. AJAX", authorName: "Михаил Давыдов"},
-{date: "29.09.12", title: "JS. Асинхронность", authorName: "Михаил Давыдов"},
-{date: "02.10.12", title: "Отладка кода", authorName: "Алексей Андросов",
+
+{date: "29.09.2012 12:00", title: "JS. Базовые знания", authorName: "Михаил Давыдов"},
+
+{date: "29.09.2012 13:00", title: "Транспорт. AJAX", authorName: "Михаил Давыдов"},
+{date: "29.09.2012 14:00", title: "JS. Асинхронность", authorName: "Михаил Давыдов"},
+
+
+{date: "02.10.2012 19:00", title: "Отладка кода", authorName: "Алексей Андросов",
 note: "Firebug, Debugbar, Web inspector"},
 
-{date: "04.10.12", title: "Клиентская оптимизация", authorName: "Иван Карев",
+
+{date: "04.10.2012 19:00", title: "Клиентская оптимизация", authorName: "Иван Карев",
 note: "YSlow"},
 
-{date: "04.10.12", title: "Profiler", authorName: "Михаил Корепанов"},
+{date: "04.10.2012 20:00", title: "Profiler", authorName: "Михаил Корепанов"},
 
-{date: "06.10.12", title: "Регулярные выражения", authorName: "Максим Ширшин"},
 
-{date: "06.10.12", title: "CSS", authorName: "Михаил Трошев"},
+{date: "06.10.2012 19:00", title: "Регулярные выражения", authorName: "Максим Ширшин"},
 
-{date: "09.10.12", title: "Фреймворки. Обзор", authorName: "Алексей Андросов",
+{date: "06.10.2012 20:00", title: "CSS", authorName: "Михаил Трошев"},
+
+
+{date: "09.10.2012 19:00", title: "Фреймворки. Обзор", authorName: "Алексей Андросов",
 note:"jQuery, Backbone, Ember, AngularJS"},
 
-{date: "09.10.12", title: "jQuery", authorName: "Алексей Бережной"},
+{date: "09.10.2012 20:00", title: "jQuery", authorName: "Алексей Бережной"},
 
-{date: "11.10.12", title: "БЭМ (2 лекции)", authorName: "Владимир Варанкин"},
 
-{date: "13.10.12", title: "Шаблонизаторы", authorName: "Сергей Бережной"},
+{date: "11.10.2012 19:00", title: "БЭМ (2 лекции)", authorName: "Владимир Варанкин"},
 
-{date: "13.10.12", title: "Дизайн", authorName: "Константин Горский",
+
+{date: "13.10.2012 12:00", title: "Шаблонизаторы", authorName: "Сергей Бережной"},
+
+{date: "13.10.2012 13:00", title: "Дизайн", authorName: "Константин Горский",
 note: "Дизайн - не хуй собачий"},
 
-{date: "13.10.12", title: "Дизайн глазами разработчика", authorName: "Михаил Трошев"}
+{date: "13.10.2012 14:00", title: "Дизайн глазами разработчика", authorName: "Михаил Трошев"}
 ];
